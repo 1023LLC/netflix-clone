b23309def67c0468b4facb227b09572f9f99da61
julius <nyerereoffice@gmail.com>
2024-10-31 03:53:09 +0300

    Title Cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,36 @@
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+import { useRef } from 'react'
+import { useEffect } from 'react';
+
+
 
 
 const TitleCards = () => {
+
+  const cardsRef = useRef();
+
+  const handlewheel = (event)=>{
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY
+  }
+  
+  useEffect(()=>{
+    cardsRef.current.addEventListener('wheel', handlewheel)
+  },[])
+
   return (
-    <div className='titlecards'>TitleCards</div>
+    <div className='title-cards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index)=>{
+          return <div className="card" key={index}>
+            <img src={card.image} alt={card.name} />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
+    </div>
   )
 }
 
