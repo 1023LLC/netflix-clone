d76d6af8d83cefb0442b70c2f3e132ec6e39182c
julius <nyerereoffice@gmail.com>
2024-10-31 04:10:44 +0300

    More Cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 
 
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
 
   const cardsRef = useRef();
 
@@ -21,7 +21,7 @@ const TitleCards = () => {
 
   return (
     <div className='title-cards'>
-      <h2>Popular on Netflix</h2>
+      <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
         {cards_data.map((card, index)=>{
           return <div className="card" key={index}>
