ec572342c2ae15b45bc78761c2d7c2f155608497
julius <nyerereoffice@gmail.com>
2024-11-07 03:44:05 +0300

    Fetching the Trailer

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -2,6 +2,7 @@ import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 import { useRef, useState } from 'react'
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 
 
@@ -42,10 +43,10 @@ const TitleCards = ({title, category}) => {
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index)=>{
-          return <div className="card" key={index}>
+          return <Link to={`/player/${card.id}`} className="card" key={index}>
             <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt={card.backdrop_path} />
             <p>{card.original_title}</p>
-          </div>
+          </Link>
         })}
       </div>
     </div>
